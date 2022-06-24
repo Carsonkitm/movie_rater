18ebbd3d2c39260ae2943d728b6da1311946ac38
Carson Moore <cartymoore@gmail.com>
2022-06-24 10:35:53 -0600

    pw-less mongo

--- persist/mongo.js
+++ persist/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 
 function connection (name, pw, host, port, db) {
-    const connectionString = `mongodb+srv://carsonkit:Posivibesonly1224@cluster0.hcczi.mongodb.net/?retryWrites=true&w=majority`;
+    const connectionString = ``;
 
     mongoose.connection(connectionString, {
         useNewUrlParser: true,
